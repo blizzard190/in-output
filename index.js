fa36f8ad693fc3e5f07575e5aaf83c7b00cf3b5f
blizzard190 <22256@ma-web.nl>
2019-06-20 10:10:03 +0200

    Input check+test plan

--- index.js
+++ index.js
@@ -24,45 +24,46 @@ io.on('connection', function(socket){
       ks.setOption('globalDelayPressMillisec', 0);
       console.log('stop');
     }*/
-
-    switch (true) {
-      case /left/i.test(msg):
-          ks.sendKey('left');
-        break;
-      case /down/i.test(msg):
-        ks.sendKey('down');
-        break;
-      case /up/i.test(msg):
-        ks.sendKey('up');
-        break;
-      case /right/i.test(msg):
-        ks.sendKey('right');
-        break;
-      case /select/i.test(msg):
-        ks.sendKey('space');
-        break;
-      case /start/i.test(msg):
-        ks.sendkey('enter');
-        break;
-      case /x/i.test(msg):
-        ks.sendKey('s');
-        break;
-      case /b/i.test(msg):
-        ks.sendKey('x');
-        break;
-      case /y/i.test(msg):
-        ks.sendkey('a');
-        break;
-      case /a/i.test(msg):
-        ks.sendkey('z');
-        break;
-      case /l/i.test(msg):
-        ks.sendkey('q');
-        break;
-      case /r/i.test(msg):
-        ks.sendkey('w');
-        break;
-      default: return null;
+    if(msg.indexOf("/") > -1){
+      switch (true) {
+        case /left/i.test(msg):
+            ks.sendKey('left');
+          break;
+        case /down/i.test(msg):
+          ks.sendKey('down');
+          break;
+        case /up/i.test(msg):
+          ks.sendKey('up');
+          break;
+        case /right/i.test(msg):
+          ks.sendKey('right');
+          break;
+        case /select/i.test(msg):
+          ks.sendKey('space');
+          break;
+        case /start/i.test(msg):
+          ks.sendkey('enter');
+          break;
+        case /x/i.test(msg):
+          ks.sendKey('s');
+          break;
+        case /b/i.test(msg):
+          ks.sendKey('x');
+          break;
+        case /y/i.test(msg):
+          ks.sendkey('a');
+          break;
+        case /a/i.test(msg):
+          ks.sendkey('z');
+          break;
+        case /l/i.test(msg):
+          ks.sendkey('q');
+          break;
+        case /r/i.test(msg):
+          ks.sendkey('w');
+          break;
+        default: return null;
+      }
     }
   });
 });
